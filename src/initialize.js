fd8b6bd260b45a0e2a47c9649106cd8561be0c34
Ivan Robles <irae45@gmail.com>
2026-02-15 13:57:09 -0600

    implement early returns at initialization, if template is invalid or the directory already exists.

--- src/initialize.js
+++ src/initialize.js
@@ -7,20 +7,26 @@ async function initialize(sourcePath, destinationPath, name) {
   const isValidTemplate = existsSync(sourcePath);
   const isDestinationPath = existsSync(destinationPath);
 
-  if (!isValidTemplate) { console.log(colors.error('Invalid Template')); }
-  if (isDestinationPath) { console.log(colors.error('Target directory already exist!')); }
-
-  if (isValidTemplate && !isDestinationPath) {
-    console.info(`📂 ${colors.ok('Target directory doesn\'t exist')}`);
-    console.info(`🚧 ${colors.ok('Creating directory')}`);
-    mkdirSync(destinationPath, { recursive: true });
-    console.info(`📁 ${colors.ok('Finished creating directory')}`);
-    copyFilesAndDirectories(sourcePath, destinationPath);
-    renamePackageJsonName(destinationPath, name);
-    console.info(`🚀 ${colors.ok(`Finished generating your app ${colors.error(name)}`)}`);
-    console.info(`🏠 ${colors.info(`cd ${name}`)}`);
-    console.info(`🧪 ${colors.info('npm install')}`);
+  if (!isValidTemplate) {
+    console.log(colors.error('Invalid Template'));
+    return;
   }
+
+  if (isDestinationPath) {
+    console.log(colors.error('Target directory already exist!'));
+    return;
+  }
+
+  console.info(`📂 ${colors.ok('Target directory doesn\'t exist')}`);
+  console.info(`🚧 ${colors.ok('Creating directory')}`);
+  mkdirSync(destinationPath, { recursive: true });
+  console.info(`📁 ${colors.ok('Finished creating directory')}`);
+  copyFilesAndDirectories(sourcePath, destinationPath);
+  renamePackageJsonName(destinationPath, name);
+  console.info(`🚀 ${colors.ok(`Finished generating your app ${colors.error(name)}`)}`);
+  console.info(`🏠 ${colors.info(`cd ${name}`)}`);
+  console.info(`🧪 ${colors.info('npm install')}`);
+
 }
 
 export default initialize;
